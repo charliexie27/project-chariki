5e26ea403c12e5c4ea19ab6052aae3af90c9ab31
Charlie Xie <charlie.xie@mail.utoronto.ca>
2020-03-09 23:42:09 -0400

    - Added LIVE and OFFLINE indicators
    - Added stop button

--- wbs/static/js/script.js
+++ wbs/static/js/script.js
@@ -42,12 +42,23 @@
             let mediaRecorder;
             let mediaStream;
 
-            const ws = new WebSocket(
+            let ws = new WebSocket(
                 window.location.protocol.replace('https', 'wss') + '//' + // http: => ws:, https: -> wss:
                 window.location.host +
                 '/rtmp/' +
                 encodeURIComponent("rtmp://live-yto.twitch.tv/app/$STREAM_KEY$")
             );
+
+            // Change button to stop
+            $('#go_live_btn').css('display', 'none');
+            $('#stop_live_btn').css('display', 'inline-block');
+            $('#go_live_btn').prop('disabled', true);
+            $('#stop_live_btn').prop('disabled', false);
+
+            // Change overlay text to LIVE
+            $('#video-overlay').addClass('live');
+            $('#video-overlay').removeClass('offline');
+            $('#video-overlay').text('LIVE');
 
             ws.addEventListener('open', (e) => {
                 mediaStream = capturedVideoElmt.captureStream();
@@ -67,6 +78,28 @@
 
             ws.addEventListener('close', (e) => {
                 mediaRecorder.stop();
+                $('#video-overlay').addClass('offline');
+                $('#video-overlay').removeClass('live');
+                $('#video-overlay').text('OFFLINE');
+
+                $('#go_live_btn').css('display', '');
+                $('#stop_live_btn').css('display', 'none');
+                $('#go_live_btn').prop('disabled', false);
+                $('#stop_live_btn').prop('disabled', true);
+            });
+
+            $('#stop_live_btn').unbind('click');
+            $('#stop_live_btn').click(function(){
+                mediaRecorder.stop();
+                $('#video-overlay').addClass('offline');
+                $('#video-overlay').removeClass('live');
+                $('#video-overlay').text('OFFLINE');
+
+                $('#go_live_btn').css('display', 'inline-block');
+                $('#stop_live_btn').css('display', 'none');
+                $('#go_live_btn').prop('disabled', false);
+                $('#stop_live_btn').prop('disabled', true);
+                ws.close();
             });
         });
 
